65e7d1be1a751d1f19d6cbc0baf50dfe4488a2c8
iPatos <inepatrick@gmail.com>
2023-03-16 17:36:22 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of a long int:%d bytes(s)\n", sizeof (long int));
 	printf("Size of a long long int:%d bytes(s)\n", sizeof (long long int));
 	printf("Size of a float:%d bytes(s)\n", sizeof (float));
-	return (0)
+	return (0);
 }
 	
