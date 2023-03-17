520100c80686830d48cda63c25e8328bcc62888d
iPatos <inepatrick@gmail.com>
2023-03-16 19:33:30 -0700

    4-put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 
-
 /**
- * main - main entry point
  *
- * return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-{ 
+
+{
+
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
