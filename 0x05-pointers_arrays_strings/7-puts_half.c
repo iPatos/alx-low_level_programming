32b03eba0df6048c834a0c80b05af6d620184837
iPatos <inepatrick@gmail.com>
2023-03-27 18:18:04 -0700

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "stdio.h"
+/**
+  * puts_half -  prints a string
+  *
+  * @str: arg *str
+  *
+  * Return: string to stdout
+  */
+
+void puts_half(char *str)
+{
+	int length, n;
+
+	length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		n = length / 2;
+		while (length > n)
+		{
+			putchar(str[n]);
+			n++;
+		}
+	}
+	else
+	{
+		n = (length + 1) / 2;
+
+		while (length > n)
+		{
+			putchar(str[n]);
+			n++;
+		}
+	}
+	putchar('\n');
+}
