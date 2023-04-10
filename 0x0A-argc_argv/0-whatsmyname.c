749d308de9c1dac07e6d7d7ec0fae4136c4f6529
iPatos <inepatrick@gmail.com>
2023-04-10 05:29:17 -0700

    whts my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Nbr of comm line arguments
+ * @argv: Array name
+ * iPatos
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
