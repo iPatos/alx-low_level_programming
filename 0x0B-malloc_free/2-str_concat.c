b44561282705efa680a30ae1e59d5fca854aff4a
iPatos <inepatrick@gmail.com>
2023-04-11 14:27:32 -0700

    conact str

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - get ends of input and add together for size
+ * @s1: input one to concat
+ * @s2: input two to concat
+ * iPatos
+ * Return: concat of s1 and s2
+ *
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *conct;
+	int p, ci;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	p = ci = 0;
+	while (s1[p] != '\0')
+		p++;
+	while (s2[ci] != '\0')
+		ci++;
+	conct = malloc(sizeof(char) * (p + ci + 1));
+
+	if (conct == NULL)
+		return (NULL);
+	p = ci = 0;
+	while (s1[p] != '\0')
+	{
+		conct[p] = s1[p];
+		p++;
+	}
+
+	while (s2[ci] != '\0')
+	{
+		conct[p] = s2[ci];
+		p++, ci++;
+	}
+	conct[p] = '\0';
+	return (conct);
+}
