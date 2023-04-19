e76cba9b3146444744e20cef4eb8fbdefcaa742c
iPatos <inepatrick@gmail.com>
2023-04-19 15:41:54 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elements to print
+ * @action: pointer to print in regular or hex
+ * iPatos
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int p;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (p = 0; p < size; p++)
+	{
+		action(array[p]);
+	}
+}
+
